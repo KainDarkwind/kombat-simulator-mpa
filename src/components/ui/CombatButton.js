655bcae7eae27a7fb5cd3d0d027eb7ccc095cf48
kaindarkwind <74018069+KainDarkwind@users.noreply.github.com>
2021-12-29 18:26:53 -0500

    removed combatbutton console logs

--- src/components/ui/CombatButton.js
+++ src/components/ui/CombatButton.js
@@ -30,109 +30,52 @@ export default class CombatButton extends React.Component {
       }
    }
 
+   checkCriticalHit(attacker, action, target) {
+      const attackerCurrentLck = attacker.lck * action.lck;
+      const targetLck = target.lck;
+
+      let isCrit = false;
+      if (attackerCurrentLck / targetLck >= getRandomInt(1, 20)) {
+         console.log("crit!");
+
+         isCrit = true;
+         return isCrit;
+      }
+   }
+
+   resolveAttack(attacker, action, target) {
+      const attackerCurrentPwr = action.pwr + attacker.pwr;
+      let critDamage = 0;
+      if (this.checkCriticalHit(attacker, action, target)) {
+         critDamage = 10;
+      }
+
+      const targetHpLoss = Math.round(
+         attackerCurrentPwr + critDamage - target.pwr / 2
+      );
+      return targetHpLoss;
+   }
+
    rollCombat(e) {
       //   const action = e.target.id;
 
       const heroAction = this.props.action;
       const opponent = this.props.opponent;
       const hero = this.props.hero;
-
-      console.log(
-         hero.name,
-         "wants to",
-         heroAction.name,
-         "against the",
-         opponent.name
-      );
-
-      const heroCurrentPwr = heroAction.pwr + hero.pwr;
-
-      console.log("This is a", heroAction.type, "action.");
+      let opponentAction = opponent.actions[0];
+      if (getRandomInt(0, 20) < opponent.lck - hero.lck) {
+         opponentAction =
+            opponent.actions[getRandomInt(0, opponent.actions.length - 1)];
+      }
 
       if (heroAction.type === "attack") {
          //run attack(target, attacker)
-         console.log("Because this is a", heroAction.type, "action:");
-         console.log(
-            "Her power is",
-            hero.pwr,
-            "and she will have her power increased by",
-            heroAction.pwr,
-            "for a total of",
-            heroCurrentPwr
-         );
 
-         console.log("Let's check if this is a crit!");
-         const heroCurrentLck = hero.lck * heroAction.lck;
-         console.log(
-            "Her luck is",
-            hero.lck,
-            "and she will have her luck increased by",
-            heroAction.lck,
-            "for a total of",
-            heroCurrentLck
-         );
-
-         let heroCritDamage = 0;
-         let isHeroCrit = false;
-         if (heroCurrentLck >= getRandomInt(1, 20)) {
-            console.log("crit!");
-            heroCritDamage = 10;
-            isHeroCrit = true;
-         } else {
-            console.log("no crit!");
-         }
-
-         const opponentHpLoss =
-            heroCurrentPwr + heroCritDamage - opponent.pwr / 2;
-
-         console.log(opponent.name, "takes", opponentHpLoss, "damage");
-
-         let opponentAction = opponent.actions[0];
-         if (getRandomInt(0, 20) < opponent.lck) {
-            opponentAction =
-               opponent.actions[getRandomInt(0, opponent.actions.length - 1)];
-         }
-
-         console.log(opponent.name, "will try to", opponentAction.name);
-
-         console.log("Let's check if this is a crit!");
-         const opponentCurrentLck = opponent.lck * opponentAction.lck;
-         console.log(
-            "His luck is",
-            opponent.lck,
-            "and he will have his luck increased by",
-            opponentAction.lck,
-            "for a total of",
-            opponentCurrentLck
-         );
-
-         let opponentCritDamage = 0;
-         let isOpponentCrit = false;
-         if (opponentCurrentLck >= getRandomInt(1, 20)) {
-            console.log("crit!");
-            opponentCritDamage = 10;
-            isOpponentCrit = true;
-         } else {
-            console.log("no crit!");
-         }
-
-         const opponentCurrentPwr = opponent.pwr + opponentAction.pwr;
-
-         console.log(
-            "His power is",
-            opponent.pwr,
-            "and he will have his power increased by",
-            opponentAction.pwr,
-            "for a total of",
-            opponentCurrentPwr
-         );
-
-         const heroHpLoss = Math.round(
-            opponentCurrentPwr + opponentCritDamage - hero.pwr / 2
-         );
-
+         const heroHpLoss = this.resolveAttack(opponent, opponentAction, hero);
          const newHeroHp = hero.hp - heroHpLoss;
          const newHeroAp = hero.ap - heroAction.ap;
+
+         const opponentHpLoss = this.resolveAttack(hero, heroAction, opponent);
          const newOpponentHp = opponent.hp - opponentHpLoss;
 
          let isHeroDead = false;
@@ -172,93 +115,32 @@ export default class CombatButton extends React.Component {
             newHeroHp: newHeroHp,
             newHeroAp: newHeroAp,
             newOpponentHp: newOpponentHp,
-            heroCrit: isHeroCrit,
-            opponentCrit: isOpponentCrit,
+
             isHeroDead: isHeroDead,
             isOpponentDead: isOpponentDead,
          };
          this.props.setCharState(combatResolution);
-
-         console.log(hero.name, "takes", heroHpLoss, "damage");
-
-         console.log(
-            "So the opponent took",
-            opponentHpLoss,
-            "damage which was a crit,",
-            isHeroCrit,
-            "and the hero took",
-            heroHpLoss,
-            "damage which was a crit,",
-            isOpponentCrit,
-            "and it cost her",
-            heroAction.ap,
-            "AP.  Now we have to send those five values back up the chain so we can update our state in Combat.js."
-         );
       } else if (heroAction.type === "defense") {
          //run defense()
-         console.log("Because this is a", heroAction.type, "action:");
-         console.log(
-            "Her power is",
-            hero.pwr,
-            "and she will have her power increased by",
-            heroAction.pwr,
-            "for a total of",
-            heroCurrentPwr
+         const heroHpLoss = Math.max(
+            0,
+            this.resolveAttack(opponent, opponentAction, hero) - heroAction.pwr
          );
+         const newHeroHp = hero.hp - heroHpLoss;
+         const newHeroAp = hero.ap - heroAction.ap;
+
          const opponentHpLoss = 0;
-         console.log(
-            opponent.name,
-            "takes",
-            opponentHpLoss,
-            "damage again because this is a",
-            heroAction.type,
-            "action.  Our combat log shouldn't even mention it as such, but we should still send it up at the end so it can update it even though it won't actually change."
-         );
+         const newOpponentHp = opponent.hp - opponentHpLoss;
 
-         let opponentAction = opponent.actions[0];
-         if (getRandomInt(0, 20) < opponent.lck) {
-            opponentAction =
-               opponent.actions[getRandomInt(0, opponent.actions.length - 1)];
+         let isHeroDead = false;
+         if (newHeroHp <= 0) {
+            isHeroDead = true;
          }
 
-         //We should adjust this opponentAction to use the start and end index of the opponent action array rather than hard coding a 0-1.
-
-         console.log(opponent.name, "will try to", opponentAction.name);
-
-         console.log("Let's check if this is a crit!");
-         const opponentCurrentLck = opponent.lck * opponentAction.lck;
-         console.log(
-            "His luck is",
-            opponent.lck,
-            "and he will have his luck increased by",
-            opponentAction.lck,
-            "for a total of",
-            opponentCurrentLck
-         );
-
-         let opponentCritDamage = 0;
-         let isOpponentCrit = false;
-         if (opponentCurrentLck >= getRandomInt(1, 20)) {
-            console.log("crit!");
-            opponentCritDamage = 10;
-            isOpponentCrit = true;
-         } else {
-            console.log("no crit!");
+         let isOpponentDead = false;
+         if (newOpponentHp <= 0) {
+            isOpponentDead = true;
          }
-
-         const opponentCurrentPwr = opponent.pwr + opponentAction.pwr;
-
-         console.log(
-            "His power is",
-            opponent.pwr,
-            "and he will have his power increased by",
-            opponentAction.pwr,
-            "for a total of",
-            opponentCurrentPwr
-         );
-
-         const heroHpLoss =
-            opponentCurrentPwr + opponentCritDamage - heroCurrentPwr / 2;
 
          const heroCombatAction =
             hero.name +
@@ -279,38 +161,17 @@ export default class CombatButton extends React.Component {
             heroHpLoss +
             " damage.";
 
-         const newHeroHp = hero.hp - heroHpLoss;
-         const newHeroAp = hero.ap - heroAction.ap;
-         const newOpponentHp = opponent.hp - opponentHpLoss;
-
          const combatResolution = {
             heroCombatAction: heroCombatAction,
             opponentCombatAction: opponentCombatAction,
             newHeroHp: newHeroHp,
             newHeroAp: newHeroAp,
             newOpponentHp: newOpponentHp,
+
+            isHeroDead: isHeroDead,
+            isOpponentDead: isOpponentDead,
          };
          this.props.setCharState(combatResolution);
-
-         console.log(
-            "The hero should now have",
-            newHeroHp,
-            "and the opponent should now have",
-            newOpponentHp
-         );
-
-         console.log(hero.name, "takes", heroHpLoss, "damage");
-         console.log(
-            "So the opponent took",
-            opponentHpLoss,
-            "damage, and the hero took",
-            heroHpLoss,
-            "damage which was a crit,",
-            isOpponentCrit,
-            "and it cost her",
-            heroAction.ap,
-            "AP.  Now we have to send those four values back up the chain so we can update our state in Combat.js.  We didn't get a hero crit value, so maybe we should send false up for that.  We also might code the logic to understand if no crit value is sent then the action didn't have one."
-         );
       }
    }
 
